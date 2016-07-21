b7016a431d97cf2a52861f0d91b33c68c3511b1b
Chris Brown <chris.brown@skookum.com>
2016-07-21 08:37:36 -0600

    Handle edge cases

--- parenths.js
+++ parenths.js
@@ -15,10 +15,21 @@ var is_opener = has(openers);
 var is_closer = has(closers);
 
 var parenths = (line) => {
+
+  var chars = line.split('');
+
+  // edge cases
+  if (
+    chars.every(c => is_opener(c))    // all openers
+    || chars.every(c => is_closer(c)) // all closers
+  ) {
+    return false;
+  }
+
   var stack = [];
   var result = true;
 
-  line.split('').forEach(c => {
+  chars.forEach(c => {
     if (is_opener(c)) {
       stack.push(brackets[c]);
     } else if (is_closer(c)) {
@@ -28,14 +39,14 @@ var parenths = (line) => {
     }
   });
 
-  return result;;
+  return result;
 }
 
-//fs.readFileSync(process.argv[2])
-//.toString()
-//.split('\n')
-//.forEach(line => { 
-//  console.log(line, parenths(line));
-//});
+fs.readFileSync(process.argv[2])
+.toString()
+.split('\n')
+.forEach(line => {
+  console.log(line, parenths(line));
+});
 
 module.exports = parenths;
--- parenths.spec.js
+++ parenths.spec.js
@@ -26,14 +26,41 @@ test('it should match nested strings', assert => {
   var test = '([])';
   var ACTUAL = parenths(test);
   var EXPECTED = true;
-  assert.equal(ACTUAL, EXPECTED, 'matches single pair');
+  assert.equal(ACTUAL, EXPECTED, 'matches nested pairs');
 
 
   test = '({[]})';
   ACTUAL = parenths(test);
   EXPECTED = true;
-  assert.equal(ACTUAL, EXPECTED, 'matches single pair');
+  assert.equal(ACTUAL, EXPECTED, 'matches nested pairs');
 
   assert.end();
 });
 
+test('it should match consecutive pairs', assert => {
+  var test = '()[]{}';
+  var ACTUAL = parenths(test);
+  var EXPECTED = true;
+  assert.equal(ACTUAL, EXPECTED, 'matches subsequent pairs');
+
+  assert.end();
+});
+
+
+test('it should fail a mis-matched pair', assert => {
+  var test = '(]';
+  var ACTUAL = parenths(test);
+  var EXPECTED = false;
+  assert.equal(ACTUAL, EXPECTED, 'fails a mis-matched pair');
+
+  assert.end();
+});
+
+test('it should fail unclosed opens', assert => {
+  var test = '((';
+  var ACTUAL = parenths(test);
+  var EXPECTED = false;
+  assert.equal(ACTUAL, EXPECTED, 'fails an unclosed open');
+
+  assert.end();
+});
